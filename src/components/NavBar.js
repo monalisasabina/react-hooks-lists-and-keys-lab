6660fc64725957741e4a89bd1f8a09751e5efacd
Monalisa Onyango <+>
2024-08-01 00:43:28 +0300

    list and keys umesumbua

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // <a href="#home">home</a>
+  // Make sure each <a> element also gets a unique key prop.
+  const eachLink= links.map((link) => <a key={link} href={"#" + link}>{link}</a>)
+
+  return <nav>{eachLink}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  
+  const oneTechnology=technologies.map((technology) => 
+    <span key={technology}>{technology}</span> 
+
+  )
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{oneTechnology} </div>
     </div>
   );
 }
